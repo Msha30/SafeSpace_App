b2facd4128154437c12dfeda86b18e8e71ae0667
Msha30 <corunoisha@gmail.com>
2025-11-13 14:25:56 +0800

    Commit

--- app/src/main/java/com/example/safespace_app/peers/Peers.kt
+++ app/src/main/java/com/example/safespace_app/peers/Peers.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
 import com.example.safespace_app.R
 
 class Peers : Fragment() {
@@ -27,5 +28,14 @@ class Peers : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         return inflater.inflate(R.layout.fragment_peers, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navHostFragment = childFragmentManager.findFragmentById(R.id.container)
+                as NavHostFragment
+
+        val navController = navHostFragment.navController
     }
 }
